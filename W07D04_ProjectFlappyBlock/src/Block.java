05b7858acbff6164ef49ee5d7a1ca8bfd09e9146
adiscehajic <adis.cehajic@bitcamp.ba>
2015-07-03 18:04:58 +0200

    Project Flappy Block

--- W07D04_ProjectFlappyBlock/src/Block.java
+++ W07D04_ProjectFlappyBlock/src/Block.java
@@ -1,35 +1,74 @@
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JOptionPane;
+import javax.swing.Timer;
 
 public class Block {
 
 	public int x;
 	public int y;
-	
-	private static final int JUMP = 120;
-	private static final int DOWN = 5;
+	private int moveY;
+	private int ground;
+
+	private static final int JUMP = 20;
+	private static final int DOWN = 2;
 	private static final int WIDTH = 40;
-	private static final int HEIGHT = 60;
-	
-	
-	public Block(int x, int y) {
+	private static final int HEIGHT = 30;
+
+	public Block(int width, int height) {
 		super();
-		this.x = x;
-		this.y = y;
+		this.x = width;
+		this.y = height;
+		this.ground = height * 2 + 150;
 	}
 
-	public void draw(Graphics g){
-		g.fillRect(x, y, WIDTH, HEIGHT);
+	public void draw(Graphics g) {
+		g.setColor(new Color(185, 185, 185));
+		g.fill3DRect(x, (int) y, WIDTH, HEIGHT, true);
+		//g.fillRect(x, (int) y, WIDTH, HEIGHT);
+
 	}
-	
-	public void jump(){
-		y -= JUMP;
+
+	public boolean colide(Obstacles obstacle) {
+		if (obstacle.x + WIDTH < x) {
+			if (obstacle.y < y && obstacle.y + HEIGHT > y) {
+				return true;
+			} else if (obstacle.y > y && y + HEIGHT > obstacle.y) {
+				return true;
+			}
+		}
+		return false;
 	}
-	
-	public void down(){
-		y += DOWN;
+
+	public void jump() {
+
+		if (moveY > 0) {
+			moveY = 0;
+		}
+		moveY -= JUMP;
 	}
+
+	public void down() {
+		if (moveY < 20) {
+			moveY += DOWN;
+		}
+		y += moveY;
+
+	}
+
+	public boolean crash() {
+
+		if (y > ground) {
+			y = ground;
+			return true;
+		}  else {
+			return false;
+		}
+	}
+
 	
-	
-	
+
 }
--- W07D04_ProjectFlappyBlock/src/FlappyBlock.java
+++ W07D04_ProjectFlappyBlock/src/FlappyBlock.java
@@ -1,5 +1,6 @@
 import java.awt.Component;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
@@ -25,31 +26,52 @@ public class FlappyBlock extends JPanel implements ActionListener {
 	private Block mainBlock;
 	private Obstacles firstObstacle;
 	private Obstacles secondObstacle;
-	
+
 	private Obstacles thirdObstacle;
 	private Obstacles fourthObstacle;
 
 	private Timer timer;
 	private Timer timerObstacle;
-	
+	private Timer timerBlock;
+
 	private BufferedImage image;
 
 	public FlappyBlock(int width, int height) {
-		try{
-		image = ImageIO.read(new File("src\\images.jpg"));
-		 } catch (Exception ex) {
-	            System.out.println("Greska");
-	       }
-		
+		try {
+			image = ImageIO.read(new File("src/images4.jpg"));
+		} catch (Exception ex) {
+			System.out.println("Greska");
+		}
+
 		this.gameWidth = width;
 		this.gameHeight = height;
 
-		mainBlock = new Block(75, height / 2);
-		firstObstacle = new Obstacles(width + 150, height - 250, width, height, 150, false);
-		secondObstacle = new Obstacles(width + 700, height - 200, width, height, 150, false);
+		mainBlock = new Block(width / 4, height / 2 - 100);
 		
+		firstObstacle = new Obstacles(width + 150, height - 130, width, height,
+				150, false);
+		secondObstacle = new Obstacles(width + 700, height - 100, width,
+				height, 150, false);
+
 		thirdObstacle = new Obstacles(width + 150, 0, width, height, 150, true);
 		fourthObstacle = new Obstacles(width + 700, 0, width, height, 150, true);
+
+		timer = new Timer(25, this);
+		timerBlock = new Timer(5, new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				if (mainBlock.y < 0) {
+					
+					mainBlock.down();
+					timer.stop();
+					timerBlock.start();
+				}
+				
+				repaint();
+				
+			}
+			
+		});
 		
 		addKeyListener(new KeyAdapter() {
 			public void keyPressed(KeyEvent e) {
@@ -61,9 +83,6 @@ public class FlappyBlock extends JPanel implements ActionListener {
 			}
 		});
 
-		timer = new Timer(25, this);
-		timerObstacle = new Timer(250, this);
-
 		addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
 				super.mouseClicked(e);
@@ -73,20 +92,24 @@ public class FlappyBlock extends JPanel implements ActionListener {
 
 		addFocusListener(new FocusListener() {
 			public void focusLost(FocusEvent e) {
-				timer.stop();
-				timerObstacle.stop();
+				//timer.stop();
+				//timerBlock.stop();
 			}
 
 			public void focusGained(FocusEvent e) {
 				timer.start();
-				timerObstacle.start();
+				timerBlock.start();
 			}
 		});
 	}
 
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		// if (mainBlock.colide(firstObstacle)) {
+		// mainBlock.crash();
+		// }
 		g.drawImage(image, 0, 0, null);
+
 		mainBlock.draw(g);
 		firstObstacle.draw(g);
 		secondObstacle.draw(g);
@@ -95,11 +118,17 @@ public class FlappyBlock extends JPanel implements ActionListener {
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		mainBlock.down();
-		firstObstacle.move();
-		secondObstacle.move();
-		thirdObstacle.move();
-		fourthObstacle.move();
+		
+		if (mainBlock.crash()) {
+			timer.stop();
+		} else {
+			mainBlock.down();
+			firstObstacle.move();
+			secondObstacle.move();
+			thirdObstacle.move();
+			fourthObstacle.move();
+		}
+
 		repaint();
 	}
 
