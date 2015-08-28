d7409a3a793b6d5ffc5c356efe689af68d2882f5
adiscehajic <adis.cehajic@bitcamp.ba>
2015-08-28 17:47:42 +0200

    Adding play framework example

---
+++ W15D05_PlayFramework/warehouse02/app/controllers/Products.java
@@ -0,0 +1,31 @@
+package controllers;
+
+import models.Product;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.products.list;
+
+import java.util.List;
+
+/**
+ * Created by adis.cehajic on 28/08/15.
+ */
+public class Products extends Controller {
+
+    public Result list() {
+        List<Product> products = Product.findAll();
+        return ok(list.render(products));
+    }
+
+    public Result newProduct() {
+        return TODO;
+    }
+
+    public Result details(String ean) {
+        return TODO;
+    }
+
+    public Result save() {
+        return TODO;
+    }
+}
